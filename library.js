46b5d350ac8f20d54590fbda643b583e0d6136ad
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2025-04-02 16:54:47 +1300

    tidy up code by adding IIFE and closures

--- library.js
+++ library.js
@@ -5,7 +5,6 @@ const dialog = document.querySelector('dialog')
 const showButton = document.querySelector('dialog + button')
 const closeButton = document.querySelector('dialog button')
 const submit = document.querySelector('#submit')
-const myLibrary = []
 
 //refactor into class
 
@@ -29,7 +28,6 @@ class Book {
       this.read = 'yes'
       result = 'Read'
     }
-    refreshDisplay(Book, this.read)
     return result
   }
 
@@ -45,139 +43,153 @@ class Book {
   }
 }
 
-function Dashboard() {}
-
-function addBookToLibrary(title, author, pages, read, image, desc) {
-  const id = crypto.randomUUID()
-  const newTitle = new Book(title, author, id, pages, read, image, desc)
-  if (
-    myLibrary.find(
-      (title) => newTitle.title + newTitle.author === title.title + title.author
-    )
-  ) {
-    return
-  } else {
-    myLibrary.push(newTitle)
-    displayBooks()
-  }
-}
-
-function displayBooks() {
-  for (const [i, book = value] of myLibrary.entries()) {
-    const getAllTitles = document.querySelectorAll('.title')
-    const allTitles = Array.from(getAllTitles)
-    const sameTitle = allTitles.find((title) => title.textContent == book.title)
-    if (sameTitle === undefined) {
-      const bookFormat = document.createElement('div')
-      const newBook = document.createElement('div')
-      const imagePlaceholder = document.createElement('div')
-      const title = document.createElement('h2')
-      const author = document.createElement('h3')
-      const pages = document.createElement('div')
-      const read = document.createElement('div')
-      const buttonContainer = document.createElement('div')
-      const image = document.createElement('img')
-      const desc = document.createElement('p')
-      const removeBook = document.createElement('button')
-      const readBook = document.createElement('button')
-      bookFormat.classList.add('book-format')
-      buttonContainer.classList.add('button-container')
-      readBook.classList.add('read-toggle')
-      newBook.classList.add('book')
-      title.classList.add('title')
-      author.classList.add('author')
-      pages.classList.add('pages')
-      read.classList.add('read')
-      bookFormat.setAttribute('id', book.id)
-      image.src = book?.image
-      removeBook.classList.add('remove-book')
-      title.textContent = book.title
-      desc.textContent = book.desc
-      author.textContent = book.author
-      pages.textContent = `${book.pages} pages`
-      if (book.read === 'yes') {
-        read.textContent = 'Read'
-      } else {
-        read.textContent = 'Not read'
-      }
-      readBook.textContent = 'Toggle Read'
-      removeBook.textContent = 'Remove Book'
-      removeBook.dataset.id = book.id
-      container.appendChild(bookFormat)
-      bookFormat.appendChild(imagePlaceholder)
-      imagePlaceholder.appendChild(image)
-      bookFormat.appendChild(newBook)
-      newBook.appendChild(title)
-      newBook.appendChild(author)
-      newBook.appendChild(read)
-      newBook.appendChild(pages)
-      newBook.appendChild(desc)
-
-      newBook.appendChild(buttonContainer)
-      buttonContainer.appendChild(readBook)
-      buttonContainer.appendChild(removeBook)
-      readBook.addEventListener('click', () => {
-        read.textContent = book.readBook()
-        buttonContainer.insertBefore(readBook, removeBook)
-      })
+function Library() {
+  const myLibrary = []
+  const addBookToLibrary = (title, author, pages, read, image, desc) => {
+    const id = crypto.randomUUID()
+    const newTitle = new Book(title, author, id, pages, read, image, desc)
+    if (
+      myLibrary.find(
+        (title) =>
+          newTitle.title + newTitle.author === title.title + title.author
+      )
+    ) {
+      return
+    } else {
+      myLibrary.push(newTitle)
     }
   }
+
+  const getLibrary = () => myLibrary
+
+  return { addBookToLibrary, getLibrary }
 }
 
-function removeBook(id) {
-  const target = document.getElementById(`${id}`)
-  target.remove()
-  const index = myLibrary.findIndex((book) => book.id === id)
-  myLibrary.splice(index, 1)
-}
+;(function Dashboard() {
+  const library = Library()
 
-function refreshDisplay(book, read) {
-  read.textContent = book.read
-}
+  //this updates the screen .... super messy
+  const displayBooks = () => {
+    for (const [i, book = value] of library.getLibrary().entries()) {
+      const getAllTitles = document.querySelectorAll('.title')
+      const allTitles = Array.from(getAllTitles)
+      const sameTitle = allTitles.find(
+        (title) => title.textContent == book.title
+      )
+      if (sameTitle === undefined) {
+        const bookFormat = document.createElement('div')
+        const newBook = document.createElement('div')
+        const imagePlaceholder = document.createElement('div')
+        const title = document.createElement('h2')
+        const author = document.createElement('h3')
+        const pages = document.createElement('div')
+        const read = document.createElement('div')
+        const buttonContainer = document.createElement('div')
+        const image = document.createElement('img')
+        const desc = document.createElement('p')
+        const removeBook = document.createElement('button')
+        const readBook = document.createElement('button')
+        bookFormat.classList.add('book-format')
+        buttonContainer.classList.add('button-container')
+        readBook.classList.add('read-toggle')
+        newBook.classList.add('book')
+        title.classList.add('title')
+        author.classList.add('author')
+        pages.classList.add('pages')
+        read.classList.add('read')
+        bookFormat.setAttribute('id', book.id)
+        image.src = book?.image
+        removeBook.classList.add('remove-book')
+        title.textContent = book.title
+        desc.textContent = book.desc
+        author.textContent = book.author
+        pages.textContent = `${book.pages} pages`
+        if (book.read === 'yes') {
+          read.textContent = 'Read'
+        } else {
+          read.textContent = 'Not read'
+        }
+        readBook.textContent = 'Toggle Read'
+        removeBook.textContent = 'Remove Book'
+        removeBook.dataset.id = book.id
+        container.appendChild(bookFormat)
+        bookFormat.appendChild(imagePlaceholder)
+        imagePlaceholder.appendChild(image)
+        bookFormat.appendChild(newBook)
+        newBook.appendChild(title)
+        newBook.appendChild(author)
+        newBook.appendChild(read)
+        newBook.appendChild(pages)
+        newBook.appendChild(desc)
 
-addBookToLibrary(
-  'A book',
-  'an author',
-  'some',
-  'no',
-  './public/images/books.jpg',
-  'some desc'
-)
-addBookToLibrary(
-  'Fullmetal Alchemist',
-  'Hiromu Arakawa',
-  '4738',
-  'yes',
-  './public/images/fullmetal-alchemist.jpg',
-  'a manga'
-)
-addBookToLibrary(
-  'Warrior Cats: Into the Wild',
-  'Erin Hunter',
-  '320',
-  'no',
-  './public/images/warriors.jpg',
-  'cats'
-)
-
-addBook.addEventListener('click', (e) => {
-  dialog.showModal()
-})
-
-closeButton.addEventListener('click', () => {
-  dialog.close()
-})
-
-submit.addEventListener('click', (e) => {
-  const form = document.querySelectorAll('input[type="text"]')
-  const radio = document.querySelector('input[type="radio"]:checked')
-  e.preventDefault()
-  addBookToLibrary(form[0].value, form[1].value, form[2].value, radio.id)
-  for (let i = 0; i < form.length; i++) {
-    form[i].value = ''
+        newBook.appendChild(buttonContainer)
+        buttonContainer.appendChild(readBook)
+        buttonContainer.appendChild(removeBook)
+        readBook.addEventListener('click', () => {
+          read.textContent = book.readBook()
+          buttonContainer.insertBefore(readBook, removeBook)
+        })
+      }
+    }
   }
-  dialog.close()
-})
-container.addEventListener('click', (e) => {
-  if (e.target.matches('.remove-book')) removeBook(e.target.dataset.id)
-})
+
+  function removeBook(id) {
+    const target = document.getElementById(`${id}`)
+    target.remove()
+    const index = library.getLibrary().findIndex((book) => book.id === id)
+    library.getLibrary().splice(index, 1)
+  }
+
+  addBook.addEventListener('click', () => {
+    dialog.showModal()
+  })
+
+  closeButton.addEventListener('click', () => {
+    dialog.close()
+  })
+
+  submit.addEventListener('click', (e) => {
+    const form = document.querySelectorAll('input[type="text"]')
+    const radio = document.querySelector('input[type="radio"]:checked')
+    e.preventDefault()
+    library.addBookToLibrary(
+      form[0].value,
+      form[1].value,
+      form[2].value,
+      radio.id
+    )
+    for (let i = 0; i < form.length; i++) {
+      form[i].value = ''
+    }
+    displayBooks()
+    dialog.close()
+  })
+  container.addEventListener('click', (e) => {
+    if (e.target.matches('.remove-book')) removeBook(e.target.dataset.id)
+  })
+  library.addBookToLibrary(
+    'A book',
+    'an author',
+    'some',
+    'no',
+    './public/images/books.jpg',
+    'some desc'
+  )
+  library.addBookToLibrary(
+    'Fullmetal Alchemist',
+    'Hiromu Arakawa',
+    '4738',
+    'yes',
+    './public/images/fullmetal-alchemist.jpg',
+    'a manga'
+  )
+  library.addBookToLibrary(
+    'Warrior Cats: Into the Wild',
+    'Erin Hunter',
+    '320',
+    'no',
+    './public/images/warriors.jpg',
+    'cats'
+  )
+  displayBooks()
+})()
